0fe75408bff2ef3650a292b02de24627bb1afda4
Dimitri Andre <dimitri@ymagyn.com>
2023-04-06 16:08:23 +0200

    make the add button reset

--- src/MovieComponents/MovieList.jsx
+++ src/MovieComponents/MovieList.jsx
@@ -1,7 +1,7 @@
 const MovieList = (props) => {
   return (
     <div>
-      {props.MovieListState.map((movie, index) => (
+      {props.movieList.map((movie, index) => (
         <div key={index} className="p-2" style={{ color: "white" }}>
           {movie}
         </div>
--- src/MovieComponents/MoviePage.jsx
+++ src/MovieComponents/MoviePage.jsx
@@ -3,14 +3,22 @@ import MovieList from "./MovieList";
 import { useState } from "react";
 
 const MoviePage = () => {
-  const [movieListState, setMovieListState] = useState([
-    "Die Not So Hard",
-    "Harry Pot de Fleur",
-  ]);
+  const [movieListState, setMovieListState] = useState(() => {
+    return { movieList: ["Die Not So Hard", "Harry Pot de Fleur"] };
+  });
+
+  function handleAddMovie(newMovie) {
+    setMovieListState((prevState) => {
+      return {
+        ...prevState,
+        movieList: prevState.movieList.concat([newMovie]),
+      };
+    });
+  }
   return (
     <div className="container col-12 col-md-6 my-3 border">
-      <AddMovie />
-      <MovieList MovieListState={movieListState} />
+      <AddMovie handleAddMovie={handleAddMovie} />
+      <MovieList movieList={movieListState.movieList} />
     </div>
   );
 };
